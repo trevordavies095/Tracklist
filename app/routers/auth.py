b2ff14d31172fee9dbc5467cc4ae915152c9bd23
trevordavies095 <me@trevordavies095.com>
2025-08-28 17:27:14 -0400

    Revert "fix: add missing request parameter to auth setup endpoint"
    This reverts commit b11681bd52d9a4dfa3edb4ce93b7617a7d621617.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -180,7 +180,6 @@ async def disable_auth(
 
 @router.post("/api/v1/auth/setup")
 async def complete_setup(
-    request: Request,
     password: str = Form(...),
     confirm_password: str = Form(...),
     response: Response = None,
